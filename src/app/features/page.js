492062dca87ac15ae173df81177ddf42247e12e0
kohcharles <charles@creatual.io>
2024-01-10 00:52:02 +0800

    add feature page

---
+++ src/app/features/page.js
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen items-center ">
+    <Navbar  />
+    <h1>Coming Soon!</h1>
+    <Footer />
+    </main>
+  )
+}
